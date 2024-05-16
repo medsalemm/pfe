9389273b996055b1bfe98b7f48257e4eb68b2c2f
med salem <talbimohamedsalm@gmail.com>
2024-05-16 21:20:12 +0100

    creating

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'images'=>'array'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    } 
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
+}
